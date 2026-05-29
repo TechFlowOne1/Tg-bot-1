2aecb72949f475a3a3dd57cd639dc5a5710e67e5
Alexandr Schelokov <safarionego@gmail.com>
2026-05-29 19:59:16 +0300

    Updated fact_handlers

--- handlers/fact_handlers.py
+++ handlers/fact_handlers.py
@@ -8,25 +8,29 @@ router = Router()
 
 @router.message(F.text == "Рандомный факт")
 async def send_random_fact(message: Message):
-    user_history[f"fact_{message.from_user.id}"] = []
+    user_key = f"fact_{message.from_user.id}"
+    if user_key not in user_history:
+        user_history[user_key] = []
     await message.answer("Думаю...")
 
     photo = FSInputFile("assets/fact.jpg")
 
-    prompt = "Расскажи короткий и интересный научный факт"
-    fact = await ask_gpt(user_id=f"fact_{message.from_user.id}", user_message=prompt)
+    prompt = """Расскажи короткий и интересный научный факт, каждый раз выбирай разные темы
+    сначала 1 факт про космос, если пользователь спросил еще факт, то даёшь факт из другой области и так далее"""
+    fact = await ask_gpt(user_id=user_key, user_message=prompt)
 
-    await message.answer_photo(
-        photo=photo,
-        caption=fact,
+    await message.answer_photo(photo=photo)
+    await message.answer(
+        text=fact,
         reply_markup=fact_keyboard
     )
 
-@router.message(F.text == "Еще факт")
+@router.message(F.text == "Ещё факт")
 async def handle_more_fact(message: Message):
     await send_random_fact(message)
 
 @router.message(F.text == "Закончить")
 async def handle_finish(message: Message):
-    user_history[f"fact{message.from_user.id}"] = []
+    user_key = f"fact_{message.from_user.id}"
+    user_history[user_key] = []
     await message.answer("Оки, сворачиваемся!", reply_markup=kb1)
