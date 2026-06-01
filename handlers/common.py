35db90090aa5fd48e10288d0cc562988ab45ccdd
Alexandr Schelokov <safarionego@gmail.com>
2026-06-01 23:09:40 +0300

    added new welcome window

--- handlers/common.py
+++ handlers/common.py
@@ -9,19 +9,20 @@ router = Router()
 
 @router.message(Command("start"))
 async def command_start(message: types.Message):
-    await message.answer(f"Привет!, {message.chat.first_name}!", reply_markup=kb1)
+    await message.answer(text=f"Саламуля!\n"
+                              f"Это бот с поддержкой Chat GPT\n"
+                              f"Ты можешь ему задать вопрос с помощью соответствующей кнопки\n"
+                              f"Любишь поиграть? Жми кнопку \"Квиз\"\n"
+                              f"Не с кем поболтать? Не проблема, жми кнопку \"Разговор\"\n"
+                              f"Хочешь узнать что-то новое? Бот насыпет тебе Фактов!\n"
+                              f"Нужно перевести какой-то текст? Ну, кнопку ты и так видишь )", reply_markup=kb1)
 
 @router.message(F.text == "Старт")
 async def button_start(message: types.Message):
-    await message.answer(f"Привет, {message.chat.first_name}!", reply_markup=kb1)
-
-#Info
-
-@router.message(Command("info"))
-async def command_info(message: types.Message):
-    await message.answer("Это бот с подключением Chat GPT. Ты можешь воспользоваться моими функциями используя кнопки!", reply_markup=kb1)
-
-@router.message(F.text == "Инфо")
-async def button_info(message: types.Message):
-    await message.answer("Это бот с подключением Chat GPT. Ты можешь воспользоваться моими функциями используя кнопки!", reply_markup=kb1)
-
+    await message.answer(text=f"Саламуля!\n"
+                              f"Это бот с поддержкой Chat GPT\n"
+                              f"Ты можешь ему задать вопрос с помощью соответствующей кнопки\n"
+                              f"Любишь поиграть? Жми кнопку \"Квиз\"\n"
+                              f"Не с кем поболтать? Не проблема, жми кнопку \"Разговор\"\n"
+                              f"Хочешь узнать что-то новое? Бот насыпет тебе Фактов!\n"
+                              f"Нужно перевести какой-то текст? Ну, кнопку ты и так видишь )", reply_markup=kb1)
